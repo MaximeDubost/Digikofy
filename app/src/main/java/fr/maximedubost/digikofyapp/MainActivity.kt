e2e6dbc3122ecd1a6f5b6670c7eb85b025b1162d
Maxime Dubost <maxime.dubost.75@gmail.com>
2021-03-29 23:33:39 +0200

    Initialisation du projet

---
+++ app/src/main/java/fr/maximedubost/digikofyapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.maximedubost.digikofyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
